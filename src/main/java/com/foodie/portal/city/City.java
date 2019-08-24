01edf87cbbdcceb17ac1e7a027362e30e9539371
Liu Ran <ran.liu1@thoughtworks.com>
2019-08-24 15:45:47 +0800

    添加swagger注释

--- src/main/java/com/foodie/portal/city/City.java
+++ src/main/java/com/foodie/portal/city/City.java
@@ -1,15 +1,20 @@
 package com.foodie.portal.city;
 
 import cn.hutool.core.util.IdUtil;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import org.springframework.cloud.commons.util.IdUtils;
 
 @Data
 public class City {
 
+    @ApiModelProperty("城市ID")
     private String id;
+    @ApiModelProperty("城市名称")
     private String name;
+    @ApiModelProperty("城市描述")
     private String desc;
+    @ApiModelProperty("城市图片")
     private String images;
 
     public City() {
--- src/main/java/com/foodie/portal/merchant/Merchant.java
+++ src/main/java/com/foodie/portal/merchant/Merchant.java
@@ -1,17 +1,27 @@
 package com.foodie.portal.merchant;
 
 import cn.hutool.core.util.IdUtil;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
+@ApiModel
 public class Merchant {
 
+    @ApiModelProperty(value = "ID")
     private String id;
-    private String name;
+    @ApiModelProperty(value = "商家名称")
+    private String value;
+    @ApiModelProperty(value = "商家邮箱")
     private String email;
+    @ApiModelProperty(value = "商家城市")
     private String city;
+    @ApiModelProperty(value = "商家描述")
     private String desc;
+    @ApiModelProperty(value = "活动描述")
     private String activeDesc;
+    @ApiModelProperty(value = "商家图片")
     private String images;
 
     public Merchant() {
