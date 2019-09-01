84275886ef0603ca3fd36d91b4d38be1faeed83e
Liu Ran <ran.liu1@thoughtworks.com>
2019-09-01 21:30:50 +0800

    文章入库

--- src/main/java/com/foodie/portal/article/ArticleController.java
+++ src/main/java/com/foodie/portal/article/ArticleController.java
@@ -1,6 +1,7 @@
 package com.foodie.portal.article;
 
 import com.foodie.portal.commons.PageCommand;
+import com.foodie.portal.commons.Pagination;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +14,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collection;
-
 @Api(tags = "文章管理")
 @RestController
 @RequestMapping("articles")
@@ -25,13 +24,13 @@ public class ArticleController {
 
     @ApiOperation("文章列表")
     @GetMapping
-    public Collection<Article> articles(PageCommand pageCommand) {
+    public Pagination<Article> articles(PageCommand pageCommand) {
         return articleApplicationService.articles(pageCommand.getPage(), pageCommand.getSize());
     }
 
     @ApiOperation("发布文章")
     @PostMapping
-    public void addArticle(@RequestBody Article articleCommand) {
+    public void addArticle(@RequestBody CreateArticleCommand articleCommand) {
         articleApplicationService.addArticle(articleCommand);
     }
 
@@ -43,7 +42,7 @@ public class ArticleController {
 
     @ApiOperation("修改文章")
     @PatchMapping("{id}")
-    public Article updateArticle(@PathVariable String id, @RequestBody Article articleCommand) {
+    public Article updateArticle(@PathVariable String id, @RequestBody CreateArticleCommand articleCommand) {
         return articleApplicationService.updateArticle(id, articleCommand);
     }
 
