16f4dbfcf89f8de24f6dd5a6330be58a834c1e83
Liu Ran <ran.liu1@thoughtworks.com>
2019-09-16 14:33:59 +0800

    阿里云禁用了25端口，邮箱需要改成465，采用ssl通信

--- src/main/java/com/foodie/portal/mail/MailApplicationService.java
+++ src/main/java/com/foodie/portal/mail/MailApplicationService.java
@@ -1,10 +1,12 @@
 package com.foodie.portal.mail;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class MailApplicationService {
 
@@ -12,7 +14,7 @@ public class MailApplicationService {
     private JavaMailSenderImpl mailSender;
 
     public void send(String to, String subject, String content) {
-
+        log.info("mail to : {}\n subject is: {}\n content is: {}", to, subject, content);
         SimpleMailMessage simpleMessage = new SimpleMailMessage();
         simpleMessage.setFrom(mailSender.getUsername());
         simpleMessage.setTo(to);
