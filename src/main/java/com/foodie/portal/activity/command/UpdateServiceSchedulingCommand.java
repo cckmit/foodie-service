7cc81dd198459be727a00e8e3fff5599ac8ae5a0
Liu Ran <ran.liu1@thoughtworks.com>
2019-10-12 09:32:18 +0800

    排期接口去掉人数

--- src/main/java/com/foodie/portal/activity/command/UpdateServiceSchedulingCommand.java
+++ src/main/java/com/foodie/portal/activity/command/UpdateServiceSchedulingCommand.java
@@ -1,22 +1,27 @@
 package com.foodie.portal.activity.command;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.Date;
 import java.util.List;
 
 @Data
+@ApiModel
 public class UpdateServiceSchedulingCommand {
 
     private Date serviceDate;
     /**
      * 班次
      */
-    private List<Shift> shifts;
+    private List<ShiftCommand> shifts;
 
 
+    @ApiModel
     @Data
-    public class Shift {
+    public static class ShiftCommand {
+        @ApiModelProperty("开始时间")
         private String startTime;
     }
 
