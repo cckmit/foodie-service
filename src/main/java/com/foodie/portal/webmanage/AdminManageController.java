1a382c485d2612e0dd741280295fab6216f0e614
Liu Ran <ran.liu1@thoughtworks.com>
2019-10-12 15:42:31 +0800

    banner post use json

--- src/main/java/com/foodie/portal/webmanage/AdminManageController.java
+++ src/main/java/com/foodie/portal/webmanage/AdminManageController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,7 +30,7 @@ public class AdminManageController {
 
     @ApiOperation("添加banner")
     @PostMapping("banner")
-    public Banner addBanner(AddBannerCommand bannerCommand) {
+    public Banner addBanner(@RequestBody AddBannerCommand bannerCommand) {
         return webManageApplicationService.addBanner(bannerCommand);
     }
 
