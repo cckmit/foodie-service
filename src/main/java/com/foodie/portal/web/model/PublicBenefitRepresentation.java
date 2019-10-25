1efb2913f8412c1355522781c2aa765e49f85945
Liu Ran <ran.liu1@thoughtworks.com>
2019-10-25 13:06:05 +0800

    公益返回percent.

--- src/main/java/com/foodie/portal/web/model/PublicBenefitRepresentation.java
+++ src/main/java/com/foodie/portal/web/model/PublicBenefitRepresentation.java
@@ -1,5 +1,6 @@
 package com.foodie.portal.web.model;
 
+import cn.hutool.core.util.NumberUtil;
 import lombok.Data;
 
 @Data
@@ -10,4 +11,12 @@ public class PublicBenefitRepresentation {
     private String content;
     private double totalFoundation;
     private double currentFoundation;
+    private double percent;
+
+    public double getPercent() {
+        if (totalFoundation == 0) {
+            return 0;
+        }
+        return NumberUtil.div(currentFoundation, totalFoundation, 4) * 100;
+    }
 }
--- src/main/java/com/foodie/portal/web/model/PublicBenefitSummaryRepresentation.java
+++ src/main/java/com/foodie/portal/web/model/PublicBenefitSummaryRepresentation.java
@@ -1,5 +1,6 @@
 package com.foodie.portal.web.model;
 
+import cn.hutool.core.util.NumberUtil;
 import lombok.Data;
 
 @Data
@@ -10,4 +11,13 @@ public class PublicBenefitSummaryRepresentation {
     private String description;
     private double totalFoundation;
     private double currentFoundation;
+    private double percent;
+
+    public double getPercent() {
+        if (totalFoundation == 0) {
+            return 0;
+        }
+        return NumberUtil.div(currentFoundation, totalFoundation, 4) * 100;
+    }
+
 }
