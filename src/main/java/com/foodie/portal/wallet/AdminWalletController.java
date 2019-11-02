3eb2a9eec01935ef6e5316868a3867afc3cea8bc
Liu Ran <ran.liu1@thoughtworks.com>
2019-11-02 17:50:56 +0800

    餐馆价格显示

---
+++ src/main/java/com/foodie/portal/wallet/AdminWalletController.java
@@ -0,0 +1,13 @@
+package com.foodie.portal.wallet;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("admin/wallet")
+public class AdminWalletController {
+
+    public void list() {
+
+    }
+}
--- src/main/java/com/foodie/portal/web/model/RestaurantRepresentation.java
+++ src/main/java/com/foodie/portal/web/model/RestaurantRepresentation.java
@@ -1,18 +1,29 @@
 package com.foodie.portal.web.model;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.foodie.portal.commons.utils.JsonUtils;
+import com.foodie.portal.restaurant.model.SetMeal;
 import com.foodie.portal.restaurant.repository.RestaurantEntity;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 public class RestaurantRepresentation {
     private String id;
     private String title;
     private String subTitle;
+    private String setMeals;
     private String images;
     private String content;
-    private Double price;
     private String cityName;
     private boolean favourite;
+
+    public Double getPrice() {
+        List<SetMeal> setMeals = JsonUtils.toBean(this.setMeals, new TypeReference<List<SetMeal>>() {
+        });
+        return setMeals.stream().mapToDouble(SetMeal::getPrice).min().getAsDouble();
+    }
 
     public static RestaurantRepresentation from(RestaurantEntity entity) {
         return RestaurantRepresentationMapper.INSTANCE.from(entity);
