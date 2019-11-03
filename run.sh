e0e6163103e5e1fd93d1aa98078c9a06585ba286
Liu Ran <ran.liu1@thoughtworks.com>
2019-11-03 19:06:59 +0800

    打印环境变量

--- run.sh
+++ run.sh
@@ -14,7 +14,8 @@ start()
 {
     echo "start $Jar_Path"
     which java
-    java -Djava.security.egd=file:/dev/.urandom -jar $Jar_Path --spring.profiles.active=sit --jasypt.encryptor.password=${JASYPT_PASSWORD} >$Jar_Path.log &
+    java -Djava.security.egd=file:/dev/.urandom -jar $Jar_Path --spring.profiles.active=sit --jasypt.encryptor.password=${JASYPT_PASSWORD} >$Jar_Path.log
+    echo $?
 }
 
 stop()
