c300a9e822365bedfdc77d918b7d6531984a16d8
Liu Ran <ran.liu1@thoughtworks.com>
2019-11-05 18:20:37 +0800

    banner 增加subTitle

--- src/main/java/com/foodie/portal/webmanage/model/Banner.java
+++ src/main/java/com/foodie/portal/webmanage/model/Banner.java
@@ -3,24 +3,30 @@ package com.foodie.portal.webmanage.model;
 import cn.hutool.core.util.IdUtil;
 import lombok.Data;
 
+import java.time.Instant;
+
 @Data
 public class Banner {
     private String id;
     private String url;
     private String title;
+    private String subTitle;
     private String link;
+    private Instant createdAt;
 
     public Banner() {
+        createdAt = Instant.now();
     }
 
-    public Banner(String title, String url, String link) {
+    public Banner(String title, String subTitle, String url, String link) {
         this.id = IdUtil.fastSimpleUUID();
+        this.subTitle = subTitle;
         this.title = title;
         this.url = url;
         this.link = link;
     }
 
-    public static Banner create(String title, String url, String link) {
-        return new Banner(title, url, link);
+    public static Banner create(String title, String subTitle, String url, String link) {
+        return new Banner(title, subTitle, url, link);
     }
 }
